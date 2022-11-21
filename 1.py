070e77c17cdc9a6c73905eebf4489d0d670db443
saireshma-27 <118727741+saireshma-27@users.noreply.github.com>
2022-11-21 15:47:08 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+def convertToTernary(N):
+	if (N == 0):
+		return
+	x = N % 3
+	N //= 3
+	if (x < 0):
+		N += 1
+	convertToTernary(N)
+	if (x < 0):
+		print(x + (3 * -1), end = "")
+	else:
+		print(x, end = "")
+def convert(Decimal):
+	if (Decimal != 0):
+		convertToTernary(Decimal)
+	else:
+		print("0", end = "")
+Decimal = int(input())
+convert(Decimal)
